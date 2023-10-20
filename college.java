40d85c25e9c9ceec6cfbceb9e2bc385edf68aa67
EnoughCorgi <116217641+EnoughCorgi@users.noreply.github.com>
2023-10-20 13:35:54 -0700

    Create college.java

---
+++ college.java
@@ -0,0 +1,13 @@
+public boolean college(int avgTemp, int numStudents, boolean csMajor){
+    //declare lovcal boolean variable
+
+     if(avgTemp <= 80)
+        //do something here
+     if(numStudents <= 8000)
+        //do something here
+     if(!csMajor)
+        //do something here
+
+   //return local variable
+    return ________;
+}
